8f80b4c2aa52d3d52eb65f20773c823058d27ddb
unknown <minhae7942@gmail.com>
2018-11-23 00:04:04 +0900

    2~9으로 제한한 구구단, y/n을 받아서 지속 결정

--- day3_181106_multiplication.py
+++ day3_181106_multiplication.py
@@ -1,14 +1,19 @@
 a="Y"
 while a=="y" or a=="Y":
     # input 받기
-    num=int(input("What number of table do you want? "))
-    #num이 숫자가 아니면?재요청
-    if(num!=int()):
-        num=int(input("type in number only"))
-    else
-        # i가 0~9인동안 출력
-        for i in range(1,10):
+    num=int(input("What number of table do you want?(2~9 only)"))
+    # 2~9사이 값이 아닌경우, 재요청
+    while num<2 or num>9:
+        print("2~9 only")
+        num=int(input("What number of table do you want?(2~9 only)"))
+    pass
+
+    # i가 0~9인동안 출력
+    for i in range(1,10):
             print("{} x {} = {}".format(num,i,num*i))
     # 지속 여부?
     a=input("Continue? (Y/N)")
+    while a!="n" and a!="N" and a!="y" and a!="Y" :
+        a=input("Continue? (Y/N)")
+    pass
 pass
